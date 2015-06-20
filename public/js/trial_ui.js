77ef348509db5a38dce08eddfa4fdc86687dd7a5
Scott Woodend <woodendscott@gmail.com>
2015-06-20 15:26:12 -0400

    - Add structure to homepage - Minor whitespace changes to trial_ui.js

--- public/js/trial_ui.js
+++ public/js/trial_ui.js
@@ -1,13 +1,11 @@
-(function(){
+(function () {
 
   $('#start-button').on('click', function () {
     var startButton = $(this).attr('disabled', true);
     $.publish('startTrials');
-    $.subscribe('endTrials', function(){
+    $.subscribe('endTrials', function () {
       startButton.removeAttr('disabled');
     })
   })
-    
-
 
 })();
