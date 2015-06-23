39760ffa0bf80947e9fb6f7998c75ea6d6fc85a3
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2015-06-23 13:55:53 -0400

    Set up initial generate model function

--- public/js/generate_initial_model.js
+++ public/js/generate_initial_model.js
@@ -4,10 +4,8 @@ var fs = require('fs');
 // use json to stringify the model
 // write that string to the file, models/0.json
 
-var model = [];
 
-var config = {
-  dimensions: [{
+var config = [{
     name: 'angleToNearestAsteroid',
     min: -180,
     max: 180,
@@ -17,5 +15,21 @@ var config = {
     min: 0,
     max: 500,
     stepSize: 10
-  }]
-};
+  }];
+
+function generateModel(listOfDims) {
+  var dim = listOfDims[0];
+  var res = [];
+  for (var i = dim.min; i < dim.max; i+= dim.stepSize) {
+    if (listOfDims.length > 1) {
+      res.push(generateModel(listOfDims.slice(1)));
+    } else {
+      res.push(0);
+    }
+  }
+  return res;
+}
+
+var model = generateModel(config);
+
+fs.writeFile('models/0.json', JSON.stringify(model));
