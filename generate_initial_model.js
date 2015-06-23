ae50d05a094b4f827e605bdd63cfc2bacab6d363
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2015-06-23 16:52:58 -0400

    Populate model 0 with predetermined data

--- generate_initial_model.js
+++ generate_initial_model.js
@@ -1,30 +1,34 @@
 var fs = require('fs');
 
-// model is a multidimensional array of classifications
-// use json to stringify the model
-// write that string to the file, models/0.json
-
-
 var config = [{
     name: 'angleToNearestAsteroid',
-    min: -180,
-    max: 180,
+    min: 0,
+    max: 359,
     stepSize: 10
   }, {
     name: 'distanceToNearestAsteroid',
     min: 0,
     max: 500,
     stepSize: 10
-  }];
+}];
 
-function generateModel(listOfDims) {
+function generateModel(listOfDims, pos, depth) {
+  pos = pos || [];
+  depth = depth || 0;
   var dim = listOfDims[0];
   var res = [];
   for (var i = dim.min; i < dim.max; i+= dim.stepSize) {
+    pos[depth] = i;
     if (listOfDims.length > 1) {
-      res.push(generateModel(listOfDims.slice(1)));
+      res.push(generateModel(listOfDims.slice(1), pos, depth + 1));
     } else {
-      res.push(0);
+      if (pos[0] < 90) {
+        res.push(3);
+      } else if (pos[0] >= 90 && pos[0] < 270) {
+        res.push(4);
+      } else if (pos[0] >= 270) {
+        res.push(2);
+      }
     }
   }
   return res;
